196cb5a33faad6d6ae870973d1b05beaa4b38a11
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-07-19 13:52:33 +0000

    up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+{  
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",         // → passe de error à warn
+    "@typescript-eslint/no-explicit-any": "warn",         // idem
+
+  }
+}
 ];
 
 export default eslintConfig;
