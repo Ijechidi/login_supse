bda65ad95373077a3f5dad5c53405587296fb9e5
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-07-19 15:11:59 +0000

    up

--- src/app/(health)/page.tsx
+++ src/app/(health)/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>f </div>
+    <div>fuck </div>
   )
 }
--- src/app/(health)/rendez-vous/@modal/(.)new-visit/[id]/page.tsx
+++ src/app/(health)/rendez-vous/@modal/(.)new-visit/[id]/page.tsx
@@ -5,5 +5,5 @@
 export default async function ModalPage({ params }: { params: Promise<{ id: string }> }) {
   const param = await params
   // return <ModalRendevous id={param.id} />;
-  return null
+  return <div> n </div>
 }
