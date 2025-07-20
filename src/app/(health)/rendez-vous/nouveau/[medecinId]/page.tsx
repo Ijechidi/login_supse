0886030fa868eead95b0939217f9c363c4994f81
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-07-20 09:55:21 +0000

    up

--- src/app/(health)/rendez-vous/nouveau/[medecinId]/page.tsx
+++ src/app/(health)/rendez-vous/nouveau/[medecinId]/page.tsx
@@ -11,7 +11,12 @@ const typesRendezVous: RendezVous[] = [
 
 ]
 // @ts-ignore 
-export default function Page() {
+export default function MedecinPage({
+  params,
+}: {
+  params: { medecinId: string } | Promise<{ medecinId: string }>
+}) {
+
   return (
     <main className="max-w-full w-full border mx-auto lg:px-12 p-6">
     
