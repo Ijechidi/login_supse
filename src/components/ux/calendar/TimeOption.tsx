c13cd4604044ab79ae866f8ef9751f8eed1e0e80
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-07-21 13:55:25 +0000

    up

--- src/components/ux/calendar/TimeOption.tsx
+++ src/components/ux/calendar/TimeOption.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import TimeSlotView from './TimeSlotView'
 import { DisponibiliteAvecRendezVous } from '@/types/globalTypes'
 
-import ConfirmVisit from './ConfirmVisit'
 import PatientConfirmVisit from './PatientConfirmVisit'
+import { HoverSlideOverlay } from './HoverContent'
+import { SlideButton } from '@/components/tools/SlideButton'
 
 
-export default function TimeOption({slot}:{slot:DisponibiliteAvecRendezVous}) {
+export default function TimeOption({slot, patientId}:{slot:DisponibiliteAvecRendezVous, patientId?:string}) {
   return (
     <div>
 <div className='flex justify-center w-full border gap-4 pr-2 rounded bg-muted/50 items-center'>
@@ -14,7 +15,17 @@ export default function TimeOption({slot}:{slot:DisponibiliteAvecRendezVous}) {
     <TimeSlotView slot={slot}  />
 
 
+
+{ slot.rendezVous.patientId === patientId ?(
+
 <PatientConfirmVisit   statut={slot.rendezVous?.statut} href={`/rendez-vous/new-visit/${slot.id}`}  />
+):(
+  <HoverSlideOverlay text='Formulaire' href={`/rendez-vous/new-visit/${slot.id}`}>
+      <SlideButton text="prendre rendez-vous" />
+  </HoverSlideOverlay>
+)
+
+}
 
 
 </div>
